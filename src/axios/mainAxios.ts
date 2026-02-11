33d5ee208c3657be9ae3e2a436800e6ecee18b61
maria romero <mariaenriquetaromero1997@gmail.com>
2026-02-11 17:46:57 -0300

    commit

--- src/axios/mainAxios.ts
+++ src/axios/mainAxios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "https://reeparando-api.onrender.com", // URL de la Mock API
+    baseURL: import.meta.env.VITE_API_URL
 });
 
 export default api;
